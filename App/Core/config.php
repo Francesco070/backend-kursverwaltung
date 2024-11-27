d19edd76774dbc9128c615db82be911865ded4e6
Francesco Palazzo <fpa@exigo.org>
2024-11-27 13:11:33 +0100

    restart own repo

---
+++ App/Core/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'mysql',
+        'user' => 'app',
+        'password' => 'app',
+        'port' => '3306',
+        'dbname' => 'kursverwaltung',
+        'charset' => 'utf8mb4',
+    ],
+];
